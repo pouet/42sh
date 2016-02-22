b8af5f707eb99a55c611fd79585f6b6a45776685
pouet <nchrupal@student.42.fr>
2016-02-22 15:13:40 +0100

    norme

--- src/error.h
+++ src/error.h
@@ -6,7 +6,7 @@
 /*   By: nchrupal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/04 10:10:33 by nchrupal          #+#    #+#             */
-/*   Updated: 2016/02/22 12:24:22 by nchrupal         ###   ########.fr       */
+/*   Updated: 2016/02/22 15:05:42 by nchrupal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,14 @@
 # define XE06  X( E_ISNODIR,    "not a directory")
 # define XE07  X( E_BADFD,      "bad file descriptor")
 # define XE08  X( E_PERMISSION, "permission denied")
+# define XE09  X( E_NUMARG,     "numeric argument required")
+# define XE10  X( E_TOOARGS,    "too many arguments")
 # define XEUKN X( E_UNKNOWN,    "unknown error")
 # define XEMAX X( E_MAX,        "")
 
-# define EM00 XE00 XE01 XE02 XE03 XE04 XE05 XE06 XE07 XE08
-# define EMACRO EM00 XEUKN XEMAX
+# define EM00 XE00 XE01 XE02 XE03 XE04 XE05 XE06 XE07 XE08 XE09
+# define EM10 XE10
+# define EMACRO EM00 EM10 XEUKN XEMAX
 
 # define X(e, s) e,
 
--- src/ft_exit.c
+++ src/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: nchrupal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/09 16:06:02 by nchrupal          #+#    #+#             */
-/*   Updated: 2016/02/16 09:10:19 by nchrupal         ###   ########.fr       */
+/*   Updated: 2016/02/22 15:12:54 by nchrupal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,15 @@ int		ft_isnumber(char *s)
 	return (*s == '\0');
 }
 
+t_env	*error_exit(t_env *env, t_error error, char *s)
+{
+	eprintf("exit: ");
+	if (s != NULL)
+		eprintf("%s: ", s);
+	eprintf("%s\n", g_error[error]);
+	return (env);
+}
+
 t_env	*ft_exit(t_env *env, t_tree *tree)
 {
 	int			i;
@@ -36,9 +45,9 @@ t_env	*ft_exit(t_env *env, t_tree *tree)
 	else
 	{
 		n = 255;
-		if (!ft_isnumber(tree->child[i]->token->s))
-			eprintf("exit: %s: numeric argument required\n",
-					tree->child[i++]->token->s);
+		if (!ft_isnumber(tree->child[i]->token->s) &&
+			*tree->child[i]->token->s != '-')
+			return (error_exit(env, E_NUMARG, tree->child[i]->token->s));
 		else
 			n = ft_atoi(tree->child[i++]->token->s);
 		if (n > 255)
@@ -46,10 +55,7 @@ t_env	*ft_exit(t_env *env, t_tree *tree)
 		while (i < tree->nchild && tree->child[i]->token->sym == S_SEPARATOR)
 			i++;
 		if (i < tree->nchild)
-		{
-			eprintf("exit: too many arguments\n");
-			return (env);
-		}
+			return (error_exit(env, E_TOOARGS, NULL));
 	}
 	exit(n);
 }
